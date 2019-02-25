fdb6fe44434d50ac6ce57988a88aa1613dec9317
Antoine Abt <antoine@abt.im>
2019-02-25 21:23:15 +0100

    build(webpack): conf

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/app/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/public/index.html'),
+      inject: 'body'
+    }),
+    new FriendlyErrorsWebpackPlugin()
+  ],
+  devServer: {
+    contentBase: './src/public', // source of static assets
+    port: 7700,
+    quiet: true
+  }
+}
